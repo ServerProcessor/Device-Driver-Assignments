523a4a7fe254af0eb02d699538db7aec9d994e9c
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-01-31 17:57:44 +0530

    Create query_ioctl.h

---
+++ IO Control/query_ioctl.h
@@ -0,0 +1,14 @@
+#ifndef QUERY_IOCTL_H   /* If not define, then define. A command to a compiler */
+#define QUERY_IOCTL_H
+#include <linux/ioctl.h>
+ 
+typedef struct
+{
+    int status, dignity, ego;
+} query_arg_t;  /* Alias name for struct {..} */
+ 
+#define QUERY_GET_VARIABLES _IOR('q', 1, query_arg_t *)
+#define QUERY_CLR_VARIABLES _IO('q', 2)
+#define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)
+ 
+#endif
