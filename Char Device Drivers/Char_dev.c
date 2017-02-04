e4b0021cbeca24d1ca59ee8735ba0cfca0f9847a
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-02-04 14:10:28 +0530

    Update Char_dev.c

--- Char Device Drivers/Char_dev.c
+++ Char Device Drivers/Char_dev.c
@@ -43,6 +43,12 @@ int ex07_module_init(void)
     printk(KERN_ALERT "Inside function %s\n", __FUNCTION__);
     
     /* Register with the kernel and indicate that we are registering a char device driver */
+    /* register_chrdev — Register a major number for character devices.  If major == 0 this functions will dynamically allocate a
+       major and return its number. If major > 0 this function will attempt to reserve a device with the given major number and
+       will return zero on success. Returns a -ve errno on failure. The name of this device has nothing to do with the name of
+       the device in /dev. It only helps to keep track of the different owners of devices. If your module name has only one type
+       of devices it's ok to use e.g. the name of the module here. */
+
     register_chrdev(240 /* Major Number */,
                     "Simple Char Drv" /* Name of the driver */,
                     &ex07_file_operations/* File Operations */);
