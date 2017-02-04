a07293baecfc80cf9d08ad6f5a7b02f088057b0b
Saumitra Kapoor <saumitrakapoor21@gmail.com>
2017-02-04 13:32:31 +0530

    Update query_ioctl.h

--- IO Control/query_ioctl.h
+++ IO Control/query_ioctl.h
@@ -7,8 +7,8 @@ typedef struct
     int status, dignity, ego;
 } query_arg_t;  /* Alias name for struct {..} */
  
-#define QUERY_GET_VARIABLES _IOR('q', 1, query_arg_t *)
-#define QUERY_CLR_VARIABLES _IO('q', 2)
-#define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)
+#define QUERY_GET_VARIABLES _IOR('q', 1, query_arg_t *)  /* Kernel to User */
+#define QUERY_CLR_VARIABLES _IO('q', 2)   /* No data movement  */
+#define QUERY_SET_VARIABLES _IOW('q', 3, query_arg_t *)  /* User to Kernel */
  
 #endif
